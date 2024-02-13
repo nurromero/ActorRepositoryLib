a3684686fd65dcc208b40c3dacf3acc5369fe0de
Nuriye <nuriyeerdogan38@gmail.com>
2024-02-13 22:07:19 +0100

    Add project files.

---
+++ ActorRepositoryLib/Actor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActorRepositoryLib
+{
+    public class Actor
+    {
+        // Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+
+
+        public Actor(int id, string name, int birthYear)
+        {
+            Id = id;
+            Name = name;
+            BirthYear = birthYear;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, BirthYear: {BirthYear}";
+
+        }
+
+        //Validate metode til Name
+        public void ValidateName()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentException("Name cant be null.");
+            }
+
+            if(Name.Length < 4)
+            {
+                throw new ArgumentException("Name has to be atleast 4 letters long.");
+            }
+        }
+
+        //Validate metode til Year
+        public void ValidateYear()
+        {
+            if (BirthYear <= 1820)
+            {
+                throw new ArgumentException("Birthyear must be ATLEAST 1820.");
+            }
+
+        }
+    }
+}
---
+++ ActorRepositoryLib/ActorsRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActorRepositoryLib
+{
+    public class ActorsRepository
+    {
+        // Opretter en List med actors
+        private List<Actor> _actors = new List<Actor>();
+
+        //Property
+        private int _nextId = 1;
+
+
+        // Get(), returnerer alle Actor objekter i listen
+        public IEnumerable<Actor> Get()
+        {
+            return _actors;
+        }
+
+        // Adds an Actor to the repository og den assigner en unique ID
+        public Actor Add(Actor actor)
+        {
+            actor.Id = _nextId++;
+            _actors.Add(actor);
+            return actor;
+        }
+
+
+        // Deletes the Author object with the specified ID
+        // And returns the deleted actor object or null if no actor object with that ID exists.
+        public Actor Delete(int id)
+        {
+            var actor = _actors.FirstOrDefault(a => a.Id == id);
+            if (actor != null)
+            {
+                _actors.Remove(actor);
+                return actor;
+            }
+
+            return null;
+            
+        }
+
+        // Updates the Actor object with the specified ID with the data also
+        // Returns the updated Actor object, or null if no Actor object was found
+        public Actor Update(int id, Actor data)
+        {
+            var actor = _actors.FirstOrDefault(a => a.Id == id);
+            if(actor != null)
+            {
+                actor.Name = data.Name;
+                actor.BirthYear = data.BirthYear;
+                return actor;
+            }
+            return null;
+        }
+    }
+}
